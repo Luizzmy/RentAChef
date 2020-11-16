8ddf1cf0d3e836dc426bf5b22ade872fc7b3b40f
Luizzmy <luizzmy@example.com>
2020-11-16 12:00:59 -0600

    CreateEvent controllers

---
+++ routes/events.js
@@ -0,0 +1,8 @@
+const {Router}=require('express')
+const uploadPicture=require('../configs/cloudinary')
+const {createEvent, viewCreateEvent}=require('../controllers/events')
+const router=Router()
+
+router.get('/create', viewCreateEvent)
+router.post('/create', uploadPicture.single('image'), createEvent)
+router.get('/events-user')
