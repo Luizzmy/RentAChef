fe3e08dac099c244834874d757ca068c65cafa20
Luizzmy <luizzmy@example.com>
2020-11-16 16:37:26 -0600

    Update Event

--- controllers/events.js
+++ controllers/events.js
@@ -1,11 +1,27 @@
 const Event = require('../models/Event.model')
 const User=require('../models/User.model')
 
+//Event Creation
 exports.viewCreateEvent=(req, res)=>res.render('Events/createEvent')
 
 exports.createEvent= async (req,res)=>{
   const {name, type, city, state, country, capacity, start, end, description, date, foodTypes, menu}=req.body
   const picture=req.file.path
   await Event.create({name, type, city, state, country, capacity, start, end, description, date, foodTypes, menu, picture})
-  res.render('Events/events-user')
+  res.redirect('/events-user')
+}
+
+//Event Edition
+exports.viewEditEvent= async (req,res)=>{
+  const {id}=req.params
+  const event = await Event.findById(id)
+  res.render('Events/editEvent', event)
+} 
+
+exports.updateEvent=async(req,res)=>{
+  const {id}=req.params
+  const {name, type, city, state, country, capacity, start, end, description, date, foodTypes, menu}=req.body
+  const picture=req.file.path
+  await Event.findByIdAndUpdate(id,{name, type, city, state, country, capacity, start, end, description, date, foodTypes, menu},{new:true})
+  res.redirect('/events-user')
 }
--- routes/events.js
+++ routes/events.js
@@ -1,10 +1,12 @@
 const Router=require('express')
 const uploadPicture=require('../configs/cloudinary')
-const {createEvent, viewCreateEvent}=require('../controllers/events')
+const {createEvent, viewCreateEvent, updateEvent, viewEditEvent}=require('../controllers/events')
 const router=Router()
 
 router.get('/create', viewCreateEvent)
 router.post('/create', uploadPicture.single('image'), createEvent)
-router.get('/events-user')
+router.get('/edit/:id', viewEditEvent)
+
+router.post('/edit/:id', uploadPicture.single('image'), updateEvent)
 
 module.exports = router;
